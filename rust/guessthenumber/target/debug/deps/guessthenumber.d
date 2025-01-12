8b16f10668450decff2c621de270561e8caff067
danial <danial.razi2112@gmail.com>
2025-01-12 08:30:37 +0330

    add rust code to the project
    See: #1

---
+++ rust/guessthenumber/target/debug/deps/guessthenumber.d
@@ -0,0 +1,5 @@
+C:\Users\danial\Desktop\guess-the-number\rust\guessthenumber\target\debug\deps\guessthenumber.exe: src/main.rs
+
+C:\Users\danial\Desktop\guess-the-number\rust\guessthenumber\target\debug\deps\guessthenumber.d: src/main.rs
+
+src/main.rs:
